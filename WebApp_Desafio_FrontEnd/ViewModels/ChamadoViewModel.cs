c39f9c0087c17832630d1dc3f717d46d97c0be1e
LucasGalvaoDev <121260233+LucasGalvaoDev@users.noreply.github.com>
2024-09-01 20:19:51 -0300

    projeto finalizado

---
+++ WebApp_Desafio_FrontEnd/ViewModels/ChamadoViewModel.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.Globalization;
+using System.Runtime.Serialization;
+using System.Xml.Linq;
+
+namespace WebApp_Desafio_FrontEnd.ViewModels
+{
+    [DataContract]
+    public class ChamadoViewModel
+    {
+        private CultureInfo ptBR = new CultureInfo("pt-BR");
+
+        [Display(Name = "ID")]
+        [DataMember(Name = "ID")]
+        public int ID { get; set; }
+
+        [Display(Name = "Assunto")]
+        [DataMember(Name = "Assunto")]
+        [Required(ErrorMessage = "O campo Assunto é obrigatório.")]
+        [StringLength(100, ErrorMessage = "O campo Assunto deve ter no máximo 100 caracteres.")]
+        public string Assunto { get; set; }
+
+        [Display(Name = "Solicitante")]
+        [DataMember(Name = "Solicitante")]
+        [Required(ErrorMessage = "O campo Solicitante é obrigatório.")]
+        [StringLength(100, ErrorMessage = "O campo Solicitante deve ter no máximo 100 caracteres.")]
+        public string Solicitante { get; set; }
+
+        [Display(Name = "IdDepartamento")]
+        [DataMember(Name = "IdDepartamento")]
+        public int IdDepartamento { get; set; }
+
+        [Display(Name = "Departamento")]
+        [DataMember(Name = "Departamento")]
+        public string Departamento { get; set; }
+
+        [Display(Name = "DataAbertura")]
+        [DataMember(Name = "DataAbertura")]
+        [Required(ErrorMessage = "O campo Data Abertura é obrigatório.")]
+        [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy}", ApplyFormatInEditMode = true)]
+        public DateTime DataAbertura { get; set; }
+
+        [DataMember(Name = "DataAberturaWrapper")]
+        public string DataAberturaWrapper
+        {
+            get
+            {
+                return DataAbertura.ToString("d", ptBR);
+            }
+            set
+            {
+                DataAbertura = DateTime.Parse(value, ptBR);
+            }
+        }
+    }
+}
---
+++ WebApp_Desafio_FrontEnd/wwwroot/js/Departamentos/Cadastrar.js
@@ -0,0 +1,78 @@
+﻿$(document).ready(function () {
+    $('.glyphicon-calendar').closest("div.date").datepicker({
+        todayBtn: "linked",
+        keyboardNavigation: false,
+        forceParse: false,
+        calendarWeeks: false,
+        format: 'dd/mm/yyyy',
+        autoclose: true,
+        language: 'pt-BR'
+    });
+
+    $('#btnCancelar').click(function () {
+        Swal.fire({
+            html: "Deseja cancelar essa operação? O registro não será salvo.",
+            icon: "warning",
+            showCancelButton: true,
+            confirmButtonText: 'Sim, cancelar',
+            cancelButtonText: 'Não, continuar'
+        }).then(function (result) {
+            if (result.isConfirmed) {
+                history.back();
+            } else {
+                console.log("Cancelou a inclusão.");
+            }
+        });
+    });
+
+    $('#btnSalvar').click(function () {
+        if ($('#form').valid() != true) {
+            Swal.fire({
+                text: 'Por favor, corrija os erros antes de salvar.',
+                icon: 'error',
+                confirmButtonText: 'OK'
+            });
+            return;
+        }
+
+        let chamado = SerializeForm($('#form'));
+        let url = $('#form').attr('action');
+
+        $.ajax({
+            type: "POST",
+            url: url,
+            data: chamado,
+            success: function (result) {
+                Swal.fire({
+                    icon: result.success ? 'success' : 'error',
+                    title: result.success ? 'Sucesso' : 'Erro',
+                    text: result.message
+                }).then(function () {
+                    if (result.success) {
+                        window.location.href = result.redirectTo;
+                    }
+                });
+            },
+            error: function (xhr, status, error) {
+                Swal.fire({
+                    text: 'Ocorreu um erro ao processar sua solicitação.',
+                    icon: 'error',
+                    confirmButtonText: 'OK'
+                });
+            }
+        });
+    });
+
+    function SerializeForm(form) {
+        var data = {};
+        $(form).find('input, select, textarea').each(function () {
+            var input = $(this);
+            var name = input.attr('name');
+            var value = input.val();
+            if (name) {
+                data[name] = value;
+            }
+        });
+        return data;
+    }
+});
